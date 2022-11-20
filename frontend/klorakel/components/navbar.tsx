d70f3b3b4e38176156b7344c28fdbf4207ae2fdb
Kywision <kywirieck@gmail.com>
2022-11-20 09:48:27 +0100

    navbar styling

--- frontend/klorakel/components/navbar.tsx
+++ frontend/klorakel/components/navbar.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Link from "next/link";
 import styled from "styled-components";
+import { useRouter } from 'next/router'
+import styles from "../styles/Home.module.css";
 
 const Bar = styled.nav`
   font-size: 18px;
@@ -18,6 +20,14 @@ const NavLi = styled.text`
   }
   border-radius: 15px;
 `;
+const NavLi2 = styled.text`
+  padding: 10px;
+  &:hover {
+    color: black;
+    background-color: rgba(255, 255, 255, 0.5);
+  }
+  border-radius: 15px;
+`;
 const Container = styled.nav`
   display: flex;
   align: center;
@@ -26,14 +36,21 @@ const Container = styled.nav`
   padding: 10px;
 `;
 export default function Navbar() {
+  // var startpage = true;
+  const router = useRouter()
+  // const deactivate = {
+  //   marginRight: 10,
+  //   background-color: black,
+  // }
+
   return (
     <Bar>
       <Container>
-        <Link href="/">
-          <NavLi>KloFinder</NavLi>
+        <Link href="/" className={router.pathname == "/" ? styles.active : ""}>
+          <NavLi >KloFinder</NavLi>
         </Link>
-        <Link href="/heatmap">
-          <NavLi>KloCluster</NavLi>
+        <Link href="/heatmap" className={router.pathname == "/heatmap" ? styles.active : ""}>
+          <NavLi2 >KloCluster</NavLi2>
         </Link>
       </Container>
     </Bar>
